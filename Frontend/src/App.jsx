833555fbba463b7030ca5bc2307a7cf35a1abc72
Shivamsaini31 <sainishivam31122005@gmail.com>
2025-08-17 11:30:52 +0530

    tries resolving deployment issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react';
-
-import Navbar from "./components/Navbar";
-
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
-
+import Navbar from "./components/Navbar"
 import { useThemeStore } from './store/useThemeStore';
 import {Routes, Route} from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
